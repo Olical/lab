578a12e9e401e4398eed2eee6cfce3fc3f33230c
Oliver Caldwell <ollie@qubit.com>
2015-08-26 18:59:24 +0100

    Revert "Add anim shim"
    This reverts commit 279173b138ac95f7b4f3106fa14dbbeb013ac30b.

--- js/d3-to-react/main.js
+++ js/d3-to-react/main.js
@@ -93,23 +93,15 @@ var Particles = React.createClass({
   }
 })
 
-function requestAnimFrame () {
-  return window.requestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  function (callback) {
-    window.setTimeout(callback, 1000 / 60)
-  }
-}
-
 function animloop () {
   _.each(circles, function (c) {
     c.o -= 0.02
     c.r += 1.1
   })
   renderParticles()
-  requestAnimFrame(animloop)
+  window.requestAnimationFrame(animloop)
 }
+animloop()
 
 function renderParticles () {
   React.render(React.createElement(Particles, {
@@ -117,5 +109,4 @@ function renderParticles () {
   }), document.getElementById('mount-particles'))
 }
 
-animloop()
-
+renderParticles()
